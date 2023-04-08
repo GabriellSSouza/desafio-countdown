33226b9d3eeca091d45984aa50e1d9432a279a7c
Gabriel Souza <gabrieldesouza003@gmail.com>
2023-04-08 00:02:39 -0300

    iniciando script

---
+++ script.js
@@ -0,0 +1,7 @@
+const mostrar_dias = document.querySelector('.dias')
+const mostrar_horas = document.querySelector('.horas')
+const mostrar_minutos = document.querySelector('.minutos')
+const mostrar_segundos = document.querySelector('.segundos')
+
+// fazer as horas com IFs que controlam até onde eles podem chegar assim quando chegar no limite um valor é passado para outro variável. Exemplo: quando as horas chegarem a 00 o dia o dia diminuí 1.
+
