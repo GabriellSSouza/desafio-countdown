8ff85b577ab4982d83a5f0c131be300257294a65
Gabriel Souza <gabrieldesouza003@gmail.com>
2023-04-11 00:01:28 -0300

    criando função para gerar segundos

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const mostrar_segundos = document.querySelector('.segundos')
 
 // fazer as horas com IFs que controlam até onde eles podem chegar assim quando chegar no limite um valor é passado para outro variável. Exemplo: quando as horas chegarem a 00 o dia o dia diminuí 1.
 
+// duas formas de controlam o tempo. class Date e setInterval/setTimeout
+
+let Cseconds = 59;
+let Cminutos = 0;
+
+function contador(){
+    if(Cseconds<0){
+        Cseconds= 59
+        Cminutos++
+    }
+    console.log(Cseconds)
+    Cseconds--
+
+    setTimeout(contador,1000)
+}
+contador()
+
+function gerarMinutos(){
+
+   
+}
+
